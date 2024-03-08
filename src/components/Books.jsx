bb2d92e32e1d6476d6e05308ca85cded68bebc0d
Jamal Shamsi <93354040+jamalshamsi1988@users.noreply.github.com>
2024-03-09 01:35:24 +0330

    add search box. project completed

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
-import { books } from "../contants/bookData";
+import { books as booksData } from "../contants/bookData";
 import BookCard from "./BookCard";
 import SideCard from "./SideCard";
-import styles from './Books.module.css';
+import styles from "./Books.module.css";
+import SearchBox from "./SearchBox";
 
 const Books = () => {
+  const [books, setBooks] = useState(booksData);
   const [liked, setLiked] = useState([]);
+  const [search, setSearch] = useState([]);
 
   const handleLikedList = (book, status) => {
     if (status) {
@@ -15,16 +18,43 @@ const Books = () => {
       setLiked((liked) => [...liked, book]);
     }
   };
-
+  const searchHandler = () => {
+    if (search) {
+      const newBooks = booksData.filter((book) =>
+        book.title.toLowerCase().includes(search)
+      );
+      setBooks(newBooks);
+    } else {
+      setBooks(booksData);
+    }
+  };
   return (
-    <div className={styles.container} >
-      <div className={styles.cards}>
-        {books.map((book) => (
-          <BookCard key={book.id} data={book} handleLikedList={handleLikedList} />
-        ))}
+    <>
+      <SearchBox
+        search={search}
+        setSearch={setSearch}
+        searchHandler={searchHandler}
+      />
+      <div className={styles.container}>
+        <div className={styles.cards}>
+          {books.map((book) => (
+            <BookCard
+              key={book.id}
+              data={book}
+              handleLikedList={handleLikedList}
+            />
+          ))}
+        </div>
+        {!!liked.length && (
+          <div className={styles.favorite}>
+            <h4>Favorite</h4>
+            {liked.map((book) => (
+              <SideCard key={book.id} data={book} />
+            ))}
+          </div>
+        )}
       </div>
-      {!!liked.length && <div className={styles.favorite}><h4>Favorite</h4>{liked.map(book => <SideCard key={book.id} data={book} />)}</div>}
-    </div>
+    </>
   );
 };
 
